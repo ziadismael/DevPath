fe60bcd6290f03a0679dcef8c1ff9e350ecf12be
Ziad Ismael <ziad.ismael@icloud.com>
2025-08-22 16:29:07 +0300

    feat: added Post and Comment models in database

---
+++ server/models/Sequalize/Post.model.js
@@ -0,0 +1,25 @@
+import {DataTypes} from 'sequelize';
+import {sequelize} from '../../database/postgres.js';
+
+const Post = sequelize.define('Post', {
+    postID:{
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true,
+    },
+    bodyText: {
+        type: DataTypes.TEXT,
+    },
+    title: {
+        type: DataTypes.STRING,
+    },
+    mediaURL: {
+        type: DataTypes.STRING,
+        allowNull: true,
+    }
+    },
+    {
+        timestamps: false,
+    });
+
+export default Post;
