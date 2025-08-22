b5592e39fcf2fcd141202e3c4df86fdf2d0c3f19
Ziad Ismael <ziad.ismael@icloud.com>
2025-08-22 16:50:50 +0300

    feat: added relationships between the models with multiplicity

--- server/models/index.models.js
+++ server/models/index.models.js
@@ -1,10 +1,36 @@
 import User from "./Sequalize/User.model.js";
 import Project from "./Sequalize/Project.model.js";
+import TeamMember from "./Sequalize/TeamMember.model.js";
+import Team from "./Sequalize/Team.model.js";
+import Interview from "./Sequalize/Interview.model.js";
+import Post from "./Sequalize/Post.model.js";
+import Comment from "./Sequalize/Comment.model.js";
+
 
 // Associations and Relationships
+User.belongsToMany(Team, {through: TeamMember});
+Team.belongsToMany(User, {through: TeamMember});
 
+Team.hasMany(Project, {foreignKey: 'teamID'});
+Project.belongsTo(Team, {foreignKey: 'teamID'});
+
+User.hasMany(Interview, {foreignKey: 'userID'});
+Interview.belongsTo(User, {foreignKey: 'userID'});
+
+User.hasMany(Post, {foreignKey: 'userID'});
+Post.belongsTo(User, {foreignKey: 'userID'});
+
+User.hasMany(Comment, {foreignKey: 'userID'});
+Comment.belongsTo(User, {foreignKey: 'userID'});
+
+Post.hasMany(Comment, {foreignKey: 'postID'});
+Comment.belongsTo(Post, {foreignKey: 'postID'});
 
 export const models = {
     User,
     Project,
+    TeamMember,
+    Interview,
+    Post,
+    Comment,
 }
