d9a8b4460f04e9a2262aab0be67f0ce44a68c989
Ziad Ismael <ziad.ismael@icloud.com>
2025-08-22 03:00:21 +0300

    feat: Added User model schema with Postgres and Sequalize

---
+++ server/models/Sequalize/User.model.js
@@ -0,0 +1,41 @@
+import {DataTypes} from 'sequelize';
+import {sequelize} from '../../database/postgres.js';
+
+const User = sequelize.define("User", {
+    id: {
+        type: DataTypes.INTEGER,   // could also use BIGINT if you expect huge numbers
+        autoIncrement: true,
+        primaryKey: true,
+    },
+    firstName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    lastName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+    },
+    university: {
+        type: DataTypes.STRING,
+    },
+    username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    }
+},
+    {
+    timestamps: false,
+});
+
+export default User;
+
