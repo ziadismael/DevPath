141f51cbd9a6653f717d7a0b48b1d14322305db4
Ziad Ismael <ziad.ismael@icloud.com>
2025-09-10 16:08:32 +0300

    refact: adjusted the controller to use InternshipClass in consistance and re-using logic. for cleaner and simpler controller

--- server/utils/internshipSync.service.js
+++ server/utils/internshipSync.service.js
@@ -3,7 +3,6 @@ import {scrapeJobs} from "jobScraper.service.js";
 
 export async function importLinkedinJobs({ keyword, location, limit = 10, filters = {} }) {
     const scrapedJobs = await scrapeJobs({ keyword, location, limit, filters });
-    // TEMP: return scrapedJobs directly to avoid class/db crash
     // return scrapedJobs;
     return await InternshipClass.bulkCreateFromScrapedJobs(scrapedJobs);
 }
