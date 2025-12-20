9f4b41efb3112f92568964cd1aa213cd1896989c
Ziad Ismael <ziad.ismael@icloud.com>
2025-12-20 02:20:19 +0200

    feat(server): update user and media post routes for frontend integration

--- server/routes/mediaPost.routes.js
+++ server/routes/mediaPost.routes.js
@@ -1,7 +1,7 @@
-import {Router} from 'express';
-import {writePost, getFriendsPosts, readPost, getAllPosts, editPost, deletePost} from '../controllers/post.controller.js';
-import {writeComment, deleteComment} from "../controllers/comment.controller.js";
-import authorize, {authorizeAdmin} from "../middlewares/auth.middleware.js";
+import { Router } from 'express';
+import { writePost, getFriendsPosts, readPost, getAllPosts, editPost, deletePost } from '../controllers/post.controller.js';
+import { writeComment, deleteComment } from "../controllers/comment.controller.js";
+import authorize, { authorizeAdmin } from "../middlewares/auth.middleware.js";
 
 const postRouter = Router();
 
@@ -11,12 +11,12 @@ const postRouter = Router();
 postRouter.post('/', authorize, writePost);
 
 // R(ead) - GET Method
-postRouter.get('/feed', authorize ,getFriendsPosts);         // <- get all posts on user's feed sorted by newest
+postRouter.get('/feed', authorize, getFriendsPosts);         // <- get all posts on user's feed sorted by newest
 postRouter.get('/:postID', readPost);                        // <- get a specific post by it's ID including it's comments
-postRouter.get('/', authorize, authorizeAdmin, getAllPosts); // Admin-only accessible: get all posts
+postRouter.get('/', authorize, getAllPosts); // Get all posts (user-accessible)
 
 // U(Pdate) - PUT Method
-postRouter.put('/:postID',authorize, editPost);              // <- edit/update post through given ID
+postRouter.put('/:postID', authorize, editPost);              // <- edit/update post through given ID
 
 // D(elete) - DELETE Method
 postRouter.delete('/:postID', authorize, deletePost);
